43f2194f4e140e1697552578e518dc8eb3e3dfcd
Tair Sairanbekov <tairsairanbekov@Tairs-MBP.Dlink>
2024-02-27 21:06:31 +0600

    feat(upper): upper section end

--- src/components/UpperSection/UpperSection.vue
+++ src/components/UpperSection/UpperSection.vue
@@ -6,11 +6,22 @@ export default {
 <template>
   <div class="main">
     <div class="text">
-      <span class="name">Hello, I am Tair,</span><br />
-      <span class="about">Software Engineer<br />from Almaty</span>
+      <div class="mainText">
+        <span class="name">Hello, I am Tair,</span><br />
+        <span class="about">Software Engineer<br />from Almaty</span>
+      </div>
+
+      <div class="subtitle">
+        A Software Engineer from Almaty. I specialize at Front-end <br />
+        but also a Back-end enthusiast
+      </div>
+      <div class="buttons">
+        <button class="buttonMain gradientButton">GET IN TOUCH</button>
+        <button class="buttonMain">VIEW ALL WORKS</button>
+      </div>
     </div>
     <div class="imageWraper">
-      <img />
+      <img src="../../assets/images/me.jpg" class="image" />
     </div>
   </div>
 </template>
